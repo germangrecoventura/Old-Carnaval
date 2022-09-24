2c505e391acaff281bf4c92f6acd915b49be65f0
germangrecoventura <germangv1998@gmail.com>
2022-09-24 13:41:07 -0300

    Agrego diseño basico de juego martillo

--- script/TouchScreenButton.gd
+++ script/TouchScreenButton.gd
@@ -9,8 +9,8 @@ extends TouchScreenButton
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	if Input.is_action_just_pressed("left-touch"):
-	#	get_tree().get_nodes_in_group("main")[0].quitar_vida()
-		get_tree().get_nodes_in_group("main")[0].agregar_vida()
+		get_tree().get_nodes_in_group("main")[0].quitar_vida()
+	#	get_tree().get_nodes_in_group("main")[0].agregar_vida()
 		#$"../ObjetoDrag".queue_free()
 
 
