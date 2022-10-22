5786d75760995cce94f86881d0c8e149d60f9499
germangrecoventura <germangv1998@gmail.com>
2022-10-22 16:16:34 -0300

    Level failed con responsive

--- actualizado/script/InformationHammerLevel.gd
+++ actualizado/script/InformationHammerLevel.gd
@@ -1,21 +1,27 @@
-extends Node
-signal e
+extends "res://script/InformationLevel.gd"
+onready var tween = $"../Tween"
+onready var selected = true
+
+func hide_information() -> void:
+	tween.interpolate_property($Information,"modulate",Color(1,1,1,1),Color(0,0,0,0.0),1.0)
+	tween.start()
 
 func _on_Accept_pressed():
-	#get_tree().paused = false
-	#emit_signal("e")
-	#$TimerTransitionGame.start()
-	#$AnimationPlayer.play("transitionMovie")
-	#$Control/Container.set_process(false)
-	pass
+	if(selected):
+		selected = false
+		get_tree().paused = false
+		emit_signal("e")
+		$TimerTransitionGame.start()
+		selected = false
+		$AnimationPlayer.play("transitionMovie")
+		hide_information()
+	
 
 func _on_TimerTransitionGame_timeout():
-	pass
-	#self.queue_free()
+	self.queue_free()
 	
 func _on_Cancel_pressed():
-	pass
-	#get_tree().paused = false
-	#emit_signal("e")
-	#get_tree().change_scene("res://scenes/Menu.tscn")
-	#self.queue_free()
+	get_tree().paused = false
+	emit_signal("e")
+	get_tree().change_scene("res://scenes/Menu.tscn")
+	self.queue_free()
