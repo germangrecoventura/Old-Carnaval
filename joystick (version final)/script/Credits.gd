40d0eadec6bce762a9cb8a67f9a154247f0c5734
germangrecoventura <germangv1998@gmail.com>
2022-10-25 18:04:50 -0300

    Arreglo urls

--- joystick (version final)/script/Credits.gd
+++ joystick (version final)/script/Credits.gd
@@ -3,3 +3,7 @@ extends Node
 func _on_Return_pressed():
 	get_tree().change_scene("res://scenes/Menu.tscn")
 	self.queue_free()
+
+
+func _on_RichTextLabel_meta_clicked(meta):
+	OS.shell_open(str(meta))
