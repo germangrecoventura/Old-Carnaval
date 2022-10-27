022082c64d5108eac33c1bffe2e6b2c3efdacccb
germangrecoventura <germangv1998@gmail.com>
2022-10-27 07:33:26 -0300

    Refactor information level

---
+++ final 27-10/script/Ufo.gd
@@ -0,0 +1,27 @@
+extends Sprite
+
+onready var joystickLeft : VirtualJoystick = $"../UI/Control2/Virtual joystick"
+
+export var speed : float = 400
+func _process(delta: float) -> void:
+#	# Movement using the joystick output:
+	if joystickLeft and joystickLeft.is_pressed():
+		position.x += joystickLeft.get_output().y * speed * delta
+	
+	# Movement using Input functions:
+	var move := Vector2.ZERO
+	move.x = Input.get_axis("ui_left", "ui_right")
+	#move.y = Input.get_axis("ui_up", "ui_down")
+	position += move * speed * delta
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Cloud":
+		$"../UI/Control3/TouchAbduction".visible= false
+		$"../UI/Control3/TouchAbduction".set_process_input(false)
+
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Cloud":
+		$"../UI/Control3/TouchAbduction".visible= true
+		$"../UI/Control3/TouchAbduction".set_process_input(true)
