fa628d9b91d8d476dde7d432f5a21ed985c4a7b9
germangrecoventura <germangv1998@gmail.com>
2022-11-18 10:58:03 -0300

    Agrego sonido de rayo

--- script/Ufo.gd
+++ script/Ufo.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var touchAbduction = $"../ControlTouchAbduction/TouchAbduction"
 onready var statusUfo = 0
+onready var sfxThunder = $"../SFX"
 
 export var speed : float = 400
 
@@ -10,13 +11,17 @@ func _ready():
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Cloud":
-		body.get_node("Body").texture = load("res://assets/storm cloud/Cloud with ray.png")
+		var audio_file = "res://sound/thunder.ogg"
+		var sound = load(audio_file)
+		sfxThunder.stream = sound
+		sfxThunder.play()
+		body.get_node("Ray").visible = true
 		touchAbduction.visible= false
 		touchAbduction.set_process_input(false)
 
 
 func _on_Area2D_body_exited(body):
 	if body.name == "Cloud" && $"../TimerCountDown".is_stopped() && $Light/TimerAbduct.is_stopped() && $Light/TimerReturnLight.is_stopped():
-		body.get_node("Body").texture = load("res://assets/storm cloud/Cloud.png")
+		body.get_node("Ray").visible = false
 		touchAbduction.visible= true
 		touchAbduction.set_process_input(true)
